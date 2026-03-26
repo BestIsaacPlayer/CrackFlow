dad637b9c95c349611097b37b3634b747b46bfff
BestIsaacPlayer <91898925+BestIsaacPlayer@users.noreply.github.com>
2026-03-26 15:51:17 +0100

    Add task card widget class

--- lib/main.dart
+++ lib/main.dart
@@ -34,3 +34,27 @@ class Task {
 
   Task({required this.title, required this.deadline});
 }
+
+class TaskCard extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final IconData icon;
+
+  const TaskCard({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(icon),
+        title: Text(title),
+        subtitle: Text(subtitle),
+      ),
+    );
+  }
+}
