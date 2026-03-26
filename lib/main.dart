9fbe1437930b3425321e13a912047e8db18b4123
BestIsaacPlayer <91898925+BestIsaacPlayer@users.noreply.github.com>
2026-03-26 15:48:33 +0100

    Make app non-const and add a final list of tasks as a field

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final List<Task> tasks = [
+    Task(title: "Wash the dishes", deadline: "Today"),
+    Task(title: "Walk the dog", deadline: "Tomorrow"),
+    Task(title: "Take out the trash", deadline: "This month"),
+    Task(title: "Take a shower", deadline: "This week")
+  ];
 
   @override
   Widget build(BuildContext context) {
