24e58354f5a73e98a74b712a60cfb621bca6e5c4
BestIsaacPlayer <91898925+BestIsaacPlayer@users.noreply.github.com>
2026-05-14 16:23:20 +0200

    Remove redundant code from task repository

--- lib/task_repository.dart
+++ lib/task_repository.dart
@@ -2,7 +2,5 @@ import 'package:crack_flow/models/task.dart';
 
 class TaskRepository {
   static List<Task> tasks = [];
-
-  static String filter = "All";
   static String selectedFilter = "All";
 }
